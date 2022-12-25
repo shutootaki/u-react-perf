c051af0aa4a9a6c99067338e13f8c1496533f6a2
shutootaki <ootaki200109@gmail.com>
2022-12-25 19:07:13 +0900

    feat: create models

--- restapi/settings.py
+++ restapi/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
-    'api.app.ApiConfig',
+    'rest_framework',
+    'api.apps.ApiConfig',
     'corsheaders'
 ]
 
